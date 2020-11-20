d73b5970f0202faae5ff163be138094790b0f319
Axiomware <algov3@gmail.com>
2020-11-19 19:27:28 -0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# netrunr-show-devices-v3
-Program to display all connected BLE devices to a gateway
+# Netrunr - Show connected BLE devices
+
+Collect connected device data from [Axiomware's](http://www.axiomware.com) Netrunr gateways using [gapi-v3-sdk](https://github.com/axiomware/gapi-v3-sdk-js.git) JavaScript SDK
+
+This Program will illustrate the following basic API functions:
+- Install and include the SDK in a NodeJS Program.
+- Connect to a MQTT broker and collect heartbeat data.
+- Use heartbeat data to connect to every gateway.
+- Collect connected device information from each gateway
+- Two programs are provided to illustrate single and continous data capture
+
+**This example uses promises and async/await functionality present in Nodejs version 8.+**.
+
+## SDK, Documentation and examples
+- [Netrunr Gateway API V3 Documentation](http://www.axiomware.com/apidocs/index.html)
+- [Netrunr Gateway API V3 SDK](https://github.com/axiomware/gapi-v3-sdk-js.git)
+
+## Requirements
+
+- [Netrunr E24](https://www.axiomware.com/netrunr-e24-product/) gateway
+- Nodejs (see [https://nodejs.org/en/](https://nodejs.org/en/) for download and installation instructions)
+  - Nodejs version 8.x.x or higher is required due to the use of promises/async/await
+- NPM (Node package manager - part of Nodejs)   
+- Windows, MacOS or Linux computer with access to internet
+
+## Installation
+
+Clone the repo
+
+`git clone https://github.com/axiomware/netrunr-show-devices-v3.git`
+
+or download as zip file to a local directory and unzip.
+
+Install all module dependencies by running the following command inside the directory
+
+```bash
+cd netrunr-show-devices-v3
+
+npm install
+```
+
+## Optional customization before running the program
+This example uses the default setup of the Netrunr gateway:
+- The client computer is connected to the LAN port of Netrunr gateway
+- The built-in MQTT broker is used and the IP address of the gateway is `192.168.8.1`
+- The MQTT broker port is `1883`
+- The MQTT topic prefix is `netrunrfe`
+
+## Usage - Collect information in single shot
+
+Run the nodejs application:
+
+`node netrunr-show-dev.js -h "192.168.8.1" -p 1883 -t "netrunrfe"`
+
+The `tls` flag will enable operation over MQTTS. This mode will use client certificates and collect data over secure link:
+
+`node netrunr-show-dev.js -h 'yourmqtthostname.com' -p 8883 -t 'netrunrfe' --tls --ca-filename='./yourRootCA.pem' --key-filename='./your-private-key.pem.key' --crt-filename='./your-client-certificate.pem.crt'`
+
+## Usage - Collect information in continuous mode
+
+Run the nodejs application:
+
+`node netrunr-show-dev-continuous.js -h "192.168.8.1" -p 1883 -t "netrunrfe"`
+
+The `tls` flag will enable operation over MQTTS. This mode will use client certificates and collect data over secure link:
+
+`node netrunr-show-dev-continuous.js -h 'yourmqtthostname.com' -p 8883 -t 'netrunrfe' --tls --ca-filename='./yourRootCA.pem' --key-filename='./your-private-key.pem.key' --crt-filename='./your-client-certificate.pem.crt'`
+
+To force exit at any time, use:
+
+`CTRL-C` 
+
+## Error conditions/Troubleshooting
+
+- If the program fails with module not installed, make sure `npm Install` is run prior to connecting to Netrunr gateway.
+- For security reasons, Clients connected to LAN ports of Netrunr gateway have limited access to upstream network.
+- If you do not see any heartbeat activity, verify network connections and your configuration.
---
+++ netrunr-show-dev.js
@@ -0,0 +1,132 @@
+/*
+ * Example program - Show connected BLE devices - single shot mode
+ *
+ * Copyright(C) 2020 Axiomware Systems Inc..
+ * https://www.axiomware.com/
+ *
+ * Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
+ */
+
+'use strict'
+
+const minimist = require('minimist')
+const gapiV3Lib = require('gapi-v3-sdk')
+const fs = require('fs')
+const path = require('path')
+
+process.stdin.resume() // so the program will not close instantly
+
+process.on('exit', function (err) { // On exit handler
+  console.log('Goodbye!')
+})
+
+process.on('unhandledRejection', (reason, p) => { // Unhandled promise rejections.
+  console.log('Unhandled Rejection at: Promise', p)
+  // application specific handling here
+})
+
+process.on('uncaughtException', (reason, p) => { // Unhandled exceptions
+  console.log(p, 'reason:', reason)
+  // application specific handling here
+})
+
+const args = minimist(process.argv.slice(2), {
+  string: ['host', // MQTT broker IP addr
+    'port', // MQTT broker port
+    'prefix', // Topic prefix
+    'ca-filename', // Root CA file name
+    'key-filename', // client key
+    'crt-filename' // client certificate
+  ],
+  boolean: ['tls'], // true -> if TLS is needed
+  alias: { h: 'host', p: 'port', t: 'prefix' },
+  default: {
+    host: '192.168.8.1',
+    port: '1883',
+    prefix: 'netrunrfe/',
+    tls: false,
+    'ca-filename': '',
+    'key-filename': '',
+    'crt-filename': ''
+  }
+})
+
+var CA = null
+var KEY = null
+var CRT = null
+
+var gHostFE = args.host
+var gPortFE = args.port
+var gTLS = args.tls
+if (gTLS) {
+  if (args['ca-filename']) {
+    const caFQN = path.isAbsolute(args['ca-filename']) ? args['ca-filename'] : path.join(__dirname, args['ca-filename'])
+    try {
+      CA = fs.readFileSync(caFQN)
+    } catch (err) {
+      console.log(`Error reading CA file [${caFQN}]`)
+    }
+  }
+  if (args['key-filename']) {
+    const keyFQN = path.isAbsolute(args['key-filename']) ? args['key-filename'] : path.join(__dirname, args['key-filename'])
+    try {
+      KEY = fs.readFileSync(keyFQN)
+    } catch (err) {
+      console.log(`Error reading KEY file [${keyFQN}]`)
+    }
+  }
+  if (args['crt-filename']) {
+    const crtFQN = path.isAbsolute(args['crt-filename']) ? args['crt-filename'] : path.join(__dirname, args['crt-filename'])
+    try {
+      CRT = fs.readFileSync(crtFQN)
+    } catch (err) {
+      console.log(`Error reading CRT file [${crtFQN}]`)
+    }
+  }
+}
+
+var gOptionsFE = {
+  username: '',
+  password: '',
+  key: KEY,
+  cert: CRT,
+  ca: CA,
+  rejectUnauthorized: false
+}
+
+var gTopicPrefixFE = args.prefix
+
+var gwHandleList = {}
+
+const gNetrunrClient = new gapiV3Lib.GapiClient()
+main()
+
+async function main () {
+  await gNetrunrClient.init(gHostFE, gPortFE, gOptionsFE, gTopicPrefixFE, gTLS)
+
+  console.log('Collecting heartbeat data for 5 seconds....')
+  await sleep(5000)
+  const gwlist = gNetrunrClient.listGW()
+  console.log(`Found ${gwlist.length} Gateways`)
+  const devInfo = []
+  // For each gateway, get version, ping and connected devices list
+  await Promise.all(gwlist.map(async gw => {
+    if (!gNetrunrClient.getBleLinkStatus(gw.id)) {
+      gwHandleList[gw.id] = await gNetrunrClient.createBleLink(gw.id)
+      if (gNetrunrClient.getBleLinkStatus(gw.id)) {
+        const ver = await gwHandleList[gw.id].version() // get version info
+        const ping = await gwHandleList[gw.id].ping() // get ping round-trip-time
+        const show = await gwHandleList[gw.id].show(5000) // get list of connected devices
+        devInfo.push({ gwid: ping.gwid, version: ver.version, 'rtt(ms)': ping.rtt, nodes: JSON.stringify(show.nodes) })
+      }
+    }
+  }))
+  // Use promise.all to wait until all async calls return and exit
+  console.table(devInfo)
+  process.exit()
+}
+
+// Delay function with delay in milliseconds
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
